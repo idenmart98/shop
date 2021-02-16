b062438b1812df5516686ca4ecdbc205486b53d9
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-02-16 18:57:39 +0600

    Add inline img to Product

--- src/showcase/admin.py
+++ src/showcase/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
-from .models import Category,Item,Product
+from .models import Category,Item,Product,ProductImage
 # Register your models here.
 
 admin.site.register(Category)
 
 admin.site.register(Item)
 
-admin.site.register(Product)
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Новости"""
+    list_display = ("name", "price","price_sell","category","description")
+    inlines = [ProductImageAdmin]
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
+
+
+
