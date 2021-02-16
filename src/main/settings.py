2ca95a0bdee1fcd02b9c29fc8e69426acfd8b92c
malika65 <malika.satimbaeva@iaau.edu.kg>
2021-02-16 18:26:33 +0600

    Starting with celery and redis

--- src/main/settings.py
+++ src/main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'celery',
     'showcase',
     'authe',
 ]
@@ -142,3 +143,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
