457d03aae0b4bcf59ad93577ec3dc120a5e70ec3
Kumarbek-sydygaliev <kumarbekdysygaliev44@gmail.com>
2021-02-16 19:54:58 +0600

    register/login set up

---
+++ src/authe/forms.py
@@ -0,0 +1,44 @@
+from django import forms
+from .models import Customer
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(required=True)
+    username = forms.CharField(required=True)
+    email = forms.CharField(required=True)
+    phone = forms.CharField(required=True)
+
+    class Meta:
+        model = Customer
+        fields = ('email', 'username', 'password', 'phone')
+
+    username.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваш ник'
+    })
+    email.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваша почта'
+    })
+    password.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваш пароль'
+    })
+    phone.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваш номер телефона'
+    })
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = Customer
+        fields = ('username', 'password')
+    username.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваш ник',
+    })
+    password.widget.attrs.update({
+        'class':'form-control',
+        'placeholder':'Ваш пароль',
+    })
--- src/authe/views.py
+++ src/authe/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
 
+from .forms import RegisterForm, LoginForm
+from .models import MainUserManager, Customer, ConfirmCode
+from .utils import send_register_mail
+
 # Create your views here.
+
+def register(request):
+    form = RegisterForm
+
+    if request.method == 'POST':
+        save_form = RegisterForm(request.POST)
+
+        if Customer.objects.filter(email = request.POST['email'], verified=False):
+            customer = Customer.objects.get(email=request.POST['email'])
+            customer.codes.all().delete()
+            code = ConfirmCode.objects.create(customer = customer)
+            send_register_mail(f'Чтобы подтвердить почту, перейдите по ссылке http://127.0.0.1:8000/authe/register/confirm/{code.code}/', code.customer.email)
+            return render(request, 'register.html', {'form': form, 'message': 'Проверьте вашу почту'})
+
+        if save_form.is_valid():
+            customer = Customer.objects.create(
+                email = request.POST['email'],
+                username = request.POST['username'],
+                password = request.POST['password'],
+                phone = request.POST['phone'])
+            code = ConfirmCode.objects.create(customer = customer)
+            send_register_mail(f'Чтобы подтвердить почту, перейдите по ссылке http://127.0.0.1:8000/authee/register/confirm/{code.code}/', code.customer.email)
+            return render(request, 'register.html', {'form': form, 'message': 'Проверьте вашу почту'})
+
+        return render(request, 'register.html', {'form': form, 'error': 'Неправильный никнейм или пароль'})
+
+    return render(request, 'register.html', {'form': form})
+
+
+def login(request):
+    if request:
+        if request.method == 'POST':
+            print(Customer.objects.filter(username = request.POST['username'], password = request.POST['password']))
+    return render(request, 'login.html', {'form': LoginForm()})
+
+def confirm(request, code):
+    if ConfirmCode.objects.filter(code=code):
+        code = ConfirmCode.objects.get(code=code)
+        if not code.confirm:
+            code.confrim = True
+            code.save()
+            code.customer.verified = True
+            code.customer.save()
+            return render(request, 'register.html', {'form': RegisterForm, 'message': 'Ваша почта подтверждена', 'success': True})
+        return render(request, 'register.html', {'form': RegisterForm, 'message': 'Ваша почта уже подтверждена', 'success': True})
+    return render(request, 'register.html', {'form': RegisterForm, 'message': 'Ваш код устарел, либо неправильный', 'success': True})
