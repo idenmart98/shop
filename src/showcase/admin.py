87bd59ae4f997c48ede4bd343df41eb4edb65051
wazovski <malikasatimbay@gmail.com>
2021-02-28 16:22:35 +0600

    Add storage model

--- src/showcase/admin.py
+++ src/showcase/admin.py
@@ -1,20 +1,46 @@
 from django.contrib import admin
-from .models import Category,Item,Product,ProductImage
+from .models import Category,Item,Product,ProductImage,Storage,StorageProduct
+from .forms import ProductForm
 # Register your models here.
 
-admin.site.register(Category)
 
-admin.site.register(Item)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    model = Item
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    model = Storage
+
+@admin.register(StorageProduct)
+class StorageProductAdmin(admin.ModelAdmin):
+    model = StorageProduct
+
 
 
 class ProductImageAdmin(admin.StackedInline):
     model = ProductImage
 
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    """Новости"""
+    
     list_display = ("name", "price","price_sell","category","description")
     inlines = [ProductImageAdmin]
+
+    form = ProductForm
+    
+    fieldsets = (
+        (None, {
+            'fields': ("name", "price","price_sell","category","description", 'items_count',),
+        }),
+    )
 
 
 @admin.register(ProductImage)
